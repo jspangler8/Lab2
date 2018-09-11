e728bec3c6d57af41ff1131823206289e1c5c1e1
jspangler8 <jacobrs4@illinois.edu>
2018-09-11 12:02:57 -0500

    added random method

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -1,5 +1,5 @@
 import edu.illinois.cs.cs125.lib.mazemaker.Maze;
-
+import java.util.Random;
 /**
  * Solve a randomly-generated maze.
  *
@@ -34,7 +34,43 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            Random rand = new Random();
+            while (maze.canMove()) {
+                maze.move();
+            }
+            int j = rand.nextInt(1);
+            if (j == 0{
+                maze.turnLeft();
+            }
+            else {
+                maze.turnRight();
+            }
+
+        }
+          /*  while (maze.isFinished()) {
+                if (maze.canMove()) {
+                    maze.move();
+                }
+                maze.turnRight();
+                if (maze.canMove()) {
+                    maze.move();
+                }
+                else {
+                    maze.turnLeft();
+                    maze.turnLeft();
+                    if (maze.canMove()) {
+                        maze.move();
+                    }
+                    else {
+                        maze.turnRight();
+                        if (maze.canMove()) {
+                            maze.turnLeft();
+                            maze.turnLeft();
+                        }
+                    }
+                }
+            }
+
         }
 
         if (maze.isFinished()) {
@@ -42,5 +78,6 @@ public class SolveMaze {
         } else {
             System.out.println("Try again!");
         }
+        */
     }
 }
